493a029b922d5faf6be4bb00dc522fa4047257c6
Meesterdaaf <d.vanoudheusden@rocva.nl>
2023-10-18 22:42:58 +0200

    added image

--- www/index.php
+++ www/index.php
@@ -4,10 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Obuh Tools4ever</title>
+    <style>
+        img {
+            width: 20%;
+            height: 20%;
+        }
+    </style>
 </head>
 
 <body>
+    <header>
+        <img src="images/Obuh.png" alt="">
+    </header>
     <a href="tools_index.php">Overzicht gereedschap</a>
 </body>
 
