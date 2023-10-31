e4570d7e369b3630beabf2874f5343cbf809ac65
David van Oudheusden <doudheusden@novacollege.nl>
2023-10-31 13:47:55 +0100

    added comment

--- sql/tools.sql
+++ sql/tools.sql
@@ -2,38 +2,33 @@ CREATE TABLE tools (
     tool_id INT NOT NULL AUTO_INCREMENT,
     tool_name VARCHAR(50) NOT NULL,
     tool_category VARCHAR(50) NOT NULL,
-    tool_price DECIMAL(10, 2) NOT NULL,
+    tool_price INT(11) NOT NULL COMMENT 'Prices are in cents',
     tool_brand VARCHAR(50) NOT NULL,
     PRIMARY KEY (tool_id)
 );
 
-
-
-
-
-
 INSERT INTO tools (tool_name, tool_category, tool_price, tool_brand)
 VALUES
-('Hammer', 'Handgereedschap', 14.99, 'Hultafors'),
-('Schroevendraaierset', 'Handgereedschap', 19.99, 'Stanley'),
-('Moersleutelset', 'Handgereedschap', 29.99, 'Bahco'),
-('Boormachine', 'Elektrisch gereedschap', 99.99, 'Bosch'),
-('Cirkelzaag', 'Elektrisch gereedschap', 149.99, 'Makita'),
-('Slijpmachine', 'Elektrisch gereedschap', 79.99, 'Metabo'),
-('Schuurmachine', 'Elektrisch gereedschap', 39.99, 'Black & Decker'),
-('Haakse slijper', 'Elektrisch gereedschap', 89.99, 'Festool'),
-('Combinatietang', 'Handgereedschap', 12.99, 'Knipex'),
-('Waterpas', 'Meetgereedschap', 9.99, 'BMI'),
-('Rolmaat', 'Meetgereedschap', 7.99, 'Stanley'),
-('Multimeter', 'Meetgereedschap', 24.99, 'Fluke'),
-('Tegelsnijder', 'Tegelgereedschap', 49.99, 'Rubi'),
-('Voegenkrabber', 'Tegelgereedschap', 7.99, 'Norton'),
-('Kitpistool', 'Kitgereedschap', 12.99, 'Tesa'),
-('Lijmpistool', 'Lijmgereedschap', 19.99, 'Bison'),
-('Stofzuiger', 'Stofafzuiging', 119.99, 'Kärcher'),
-('Verfafbrander', 'Verfgereedschap', 39.99, 'Steinel'),
-('Verfroller', 'Verfgereedschap', 9.99, 'Anza'),
-('Plamuurmes', 'Verfgereedschap', 6.99, 'Goudhaantje');
+('Hammer', 'Handgereedschap', 1499, 'Hultafors'),
+('Schroevendraaierset', 'Handgereedschap', 1999, 'Stanley'),
+('Moersleutelset', 'Handgereedschap', 2999, 'Bahco'),
+('Boormachine', 'Elektrisch gereedschap', 9999, 'Bosch'),
+('Cirkelzaag', 'Elektrisch gereedschap', 14999, 'Makita'),
+('Slijpmachine', 'Elektrisch gereedschap', 7999, 'Metabo'),
+('Schuurmachine', 'Elektrisch gereedschap', 3999, 'Black & Decker'),
+('Haakse slijper', 'Elektrisch gereedschap', 8999, 'Festool'),
+('Combinatietang', 'Handgereedschap', 1299, 'Knipex'),
+('Waterpas', 'Meetgereedschap', 999, 'BMI'),
+('Rolmaat', 'Meetgereedschap', 799, 'Stanley'),
+('Multimeter', 'Meetgereedschap', 2499, 'Fluke'),
+('Tegelsnijder', 'Tegelgereedschap', 4999, 'Rubi'),
+('Voegenkrabber', 'Tegelgereedschap', 799, 'Norton'),
+('Kitpistool', 'Kitgereedschap', 1299, 'Tesa'),
+('Lijmpistool', 'Lijmgereedschap', 1999, 'Bison'),
+('Stofzuiger', 'Stofafzuiging', 11999, 'Kärcher'),
+('Verfafbrander', 'Verfgereedschap', 3999, 'Steinel'),
+('Verfroller', 'Verfgereedschap', 999, 'Anza'),
+('Plamuurmes', 'Verfgereedschap', 699, 'Goudhaantje');
 
 /*
 Onderstaande code gaan we ook gebruiken maar op een later moment
