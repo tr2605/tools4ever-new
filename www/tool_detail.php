ac8ced69afc011e3a88af05f5afe7a9f47d0aa5e
David van Oudheusden <doudheusden@novacollege.nl>
2024-02-07 15:57:03 +0100

    some styling

--- www/tool_detail.php
+++ www/tool_detail.php
@@ -16,13 +16,21 @@ require 'header.php';
     <div class="container">
         <?php if (isset($tool)) : ?>
             <div class="product-detail">
-                <img src="<?php echo isset($tool['tool_image']) ? 'images/' . $tool['tool_image'] : 'https://placehold.co/200' ?>" alt="<?php echo $tool['tool_name'] ?>">
-                <h3><?php echo $tool['tool_name'] ?></h3>
-                <p><?php echo $tool['tool_category'] ?></p>
-                <p>€ <?php echo $tool['tool_price'] ?></p>
-                <p>
-                    <a href="add_to_cart.php?id=<?php echo $tool['tool_id']; ?>" class="btn">Bestel</a>
-                </p>
+                <div class="row">
+                    <div class="col">
+                        <img src="<?php echo isset($tool['tool_image']) ? 'images/' . $tool['tool_image'] : 'https://placehold.co/200' ?>" alt="<?php echo $tool['tool_name'] ?>">
+                    </div>
+                    <div class="col">
+                        <h3><?php echo $tool['tool_name'] ?></h3>
+                        <p><?php echo $tool['tool_brand'] ?></p>
+                        <p><?php echo $tool['tool_category'] ?></p>
+                        <p>€ <?php echo number_format($tool['tool_price'] / 100, 2, ',', '') ?></p>
+                        <p>
+                            <a href="add_to_cart.php?id=<?php echo $tool['tool_id']; ?>" class="btn">Bestel</a>
+                        </p>
+                    </div>
+                </div>
+
             </div>
         <?php else : ?>
             <p>Tool not found.</p>
