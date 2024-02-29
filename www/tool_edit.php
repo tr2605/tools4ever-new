c06d086e7878b6e1b9b5bab33cde191ab69e2121
Tim Rogier <t.rogier@me.com>
2024-02-29 08:57:34 +0100

    blabla

---
+++ www/tool_edit.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+ 
+if (!isset($_SESSION['user_id'])) {
+    echo "You are not logged in, please login. ";
+    echo "<a href='login.php'>Login here</a>";
+    exit;
+}
+ 
+if ($_SESSION['role'] != 'admin') {
+    echo "You are not allowed to view this page, please login as admin";
+    exit;
+}
+ 
+//ZORG ERVOOR DAT JE DE INVULVELDEN VULT
+//MET DATA VAN DE GESELECTEERDE GEBRUIKER
+ 
+require 'database.php';
+require 'header.php';
+ 
+?>
+ 
+ <main>
+    <div class="container">
+        <form action="tool_update.php?id=<?php echo $_GET['id'] ?>" method="post">
+            <div>
+                <label for="tool_name">Naam:</label>
+                <input type="text" id="tool_name" name="tool_name">
+            </div>
+            <div>
+                <label for="tool_brand">Merk:</label>
+                <input type="text" id="tool_brand" name="tool_brand">
+            </div>
+            <div>
+                <label for="tool_category">Categorie:</label>
+                <input type="text" id="tool_category" name="tool_category">
+            </div>
+            <div>
+                <label for="tool_price">Prijs:</label>
+                <input type="text" id="tool_price" name="tool_price">
+            </div>
+ 
+            <input type="submit" value="Edit tool">
+        </form>
+    </div>
+</main>
+<?php require 'footer.php' ?>
+
---
+++ www/tool_update.php
@@ -0,0 +1,41 @@
+<?php
+// echo "<pre>
+
+session_start();
+require 'database.php';
+
+
+//check method
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    echo "You are not allowed to view this page";
+    exit;
+}
+
+// Check if all fields are filled in
+if (empty($_POST['tool_name']) || empty($_POST['tool_brand']) || empty($_POST['tool_category']) || empty($_POST['tool_price'])) {
+    echo "Please fill in all fields";
+    exit;
+}
+
+$id = $_GET['id'];
+// $email = $_POST['email'];
+// $password = $_POST['password'];
+$tool_name = $_POST['tool_name'];
+$tool_brand = $_POST['tool_brand'];
+$tool_category = $_POST['tool_category'];
+$tool_price = $_POST['tool_price'];
+
+$sql = "UPDATE tools SET tool_name = :tool_name, tool_brand = :tool_brand, tool_category = :tool_category, tool_price = :tool_price WHERE tool_id = :id";
+
+$stmt = $conn->prepare($sql);
+$stmt->bindParam(":id", $id);
+$stmt->bindParam(":tool_name", $tool_name);
+$stmt->bindParam(":tool_brand", $tool_brand);
+$stmt->bindParam(":tool_category", $tool_category);
+$stmt->bindParam(":tool_price", $tool_price);
+$stmt->execute();
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($stmt->execute()) {
+    echo "Something went good";
+}
